579bd2be2ea40c75a3f18fb01790b317edceef69
Gabriele Corti <gabriele.corti@ecampus.local>
2025-08-18 16:33:38 +0200

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { gsap } from "gsap"
+import {ScrollTrigger, SplitText} from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+
+const App = () => {
+  return (
+    <div>Emark</div>
+  )
+}
+
+export default App
