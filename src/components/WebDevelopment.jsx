93c6adae2a9c1b6ca8eb8de0a0c84e25d63e044a
Gabriele Corti <gabriele.corti@ecampus.local>
2025-09-04 16:53:15 +0200

    web development section

--- src/components/WebDevelopment.jsx
+++ src/components/WebDevelopment.jsx
@@ -8,8 +8,15 @@ gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 const WebDevelopment = () => {
   return (
-    <div id='web-development' className='bg-black'>
-        <ButtonServizi title="Web developer" />
+    <div id='web-development' className='bg-black w-full h-full justify-start px-30'>
+      <div>
+        <ButtonServizi title="Web development"/>
+      </div>
+      <div className='text-white columns-2 gap-10 py-10'>
+        <p className='w-100 font-bold'>Creiamo siti e landing page d’impatto, belli e funzionali con testi ottimizzati per dominare la SERP.</p>
+        <p className='w-100 font-light'>Il lavoro di eMark è uno: aiutare le aziende a raggiungere in modo ottimale i propri obiettivi.</p>
+      </div>
+        
     </div>
   )
 }
